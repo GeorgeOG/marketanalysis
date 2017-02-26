d3309b3e6643e68caa964553ea317a765b367513
GeorgeOG <george.garber@me.com>
2017-02-26 21:50:28 +0000

    initial docs

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <link rel="icon" href="favicon.png" type="image/png" />
+  <title>Market Analysis Coursework</title>
+</head>
+<body>
+  <h1>
+  <?php
+    $name='Nikita';
+    echo exec('python i.py "'.$name.'"');
+   ?>
+ </h1>
+</body>
+</html>
